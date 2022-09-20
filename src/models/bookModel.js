a946d025fa7ab8ed33a97ae8c4f869026776ff39
rahul <guptagi5050@gmail.com>
2022-09-20 13:24:58 +0530

    userSchema

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,10 +1,43 @@
 const mongoose = require("mongoose")
 
-const bookSchema = new mongoose.Schema(
-{
+const emailValidation = function(email){
+    let regexForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return regexForEmail.test(email)
+}
 
-},{
-    timestamps:true
-})
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: string,
+        required: [true, "Title is required"],
+        enum: [Mr, Mrs, Miss]
+    },
+    name: {
+        type: string,
+        required: [true, "Name is required"],
+    },
+    phone: {
+        type: string,
+        required: true,
+        unique: [true, "Phone should be unique"]
+    },
+    email: {
+        type: string,
+        required: true,
+        validate:[emailValidation,"plz enter valid email"],
+        unique: [true, "email should be unique"]
+    },
+
+    password: {
+        type: string,
+        required: [true, "password is required"],
+    },
+    address: {
+        street: { string },
+        city: { string },
+        pincode: { string }
+    }
+
+
+}, { timestamps: true })
 
 module.exports = mongoose.model('Book', bookSchema)
